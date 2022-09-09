1199cf051d00e7a11f985901bf1bc99a259f5ddc
Chinwem <franceschinwem1999@gmail.com>
2022-09-08 17:41:57 -0700

    8C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
-#include<stdio.h>
+#include<unistd.h>
 /**
- * main - print size of data
+ * main-program entry point
  *
- * Return: Always 0
+ * Return: 1 on success
  */
 int main(void)
 {
-	fputs("and that piece of art is useful" - Dora Korpar, 2015-10-19
-stdout);
+	write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59);
 	return (1);
-}	
+}
